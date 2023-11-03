07bcdd86d5a42ef73b4eede7d5916f1830788456
Alfredo Magaña <en2ruedas11@gmail.com>
2023-11-02 23:16:31 -0600

    Login

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>En la Nube</title>
+</head>
+<body>
+    <?php
+    echo "Hola Mundo";
+    ?> 
+    <form action="">
+        <label for="">Usuario</label>
+        <input type="text">
+        <label for="">Contraseña</label>
+        <input type="text">
+        <button type="submit">Aceptar</button>
+
+    </form>
+</body>
+</html>
